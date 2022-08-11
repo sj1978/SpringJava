46229611eb6fdb94731c37a42db63aefac52fac8
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-08-11 16:42:14 -0300

    Jersey config

--- src/main/java/com/famjava/famjava/config/JerseyConfig.java
+++ src/main/java/com/famjava/famjava/config/JerseyConfig.java
@@ -1,4 +1,15 @@
 package com.famjava.famjava.config;
 
-public class JerseyConfig {
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/")
+@Component
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig () {
+        this.packages("com.famjava.famjava.controllers");
+    }
 }
+
